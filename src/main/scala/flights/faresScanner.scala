c78efcb1e450256b4eae5fccda590a0ae3ea065a
SNiko <SWashchyshchuk@gmail.com>
2015-11-29 11:56:09 +1100

    The flights scan logic has been moved from Main to FaresScanner

---
+++ src/main/scala/flights/faresScanner.scala
@@ -0,0 +1,116 @@
+package flights
+
+import java.time._
+import java.time.format.DateTimeFormatter
+
+import config.{AppConfig, TripConfig}
+import utils.Implicits._
+import utils.{After, Log, Utils}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.Success
+
+class FaresScanner {
+  val MaxConcurrentLoads = 3
+  val CheckInterval = Duration.ofHours(24)
+
+  val cities = flights.ReferenceData.airports.map(a => (a.iataCode, a.city)).toMap
+
+  def getFilePath(flights: Seq[FlightDirection]): String = {
+    val route = flights.map(f => DateTimeFormatter.ofPattern("ddMM").format(f.date) + f.fromAirport + f.toAirport).mkString
+    val timestamp = DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmssZ").format(LocalDateTime.now)
+    val fileName = s"$timestamp $route.txt"
+
+    AppConfig.baseFolder + "formatted\\" + fileName
+  }
+
+  def logError(route: Seq[FlightDirection], error: String) = Log(s"An error occurred when loading '$route': $error")
+
+  def saveTripFares(allFares: Seq[Fare]) = {
+    val faresByDirection = allFares.groupBy(f => {
+      val origin = cities(f.itineraries.head.flights.head.fromAirport)
+      val destination = cities(f.itineraries.head.flights.last.toAirport)
+
+      (origin, destination)
+    })
+
+    faresByDirection.foreach {
+      case ((origin, destination), fares) =>
+        val formattedFares = fares.sortBy(_.prices.map(_.price).min).map(f => f.prettyPrint).mkString("\n")
+        val tripDate = fares.head.itineraries.head.flights.head.departureDate
+        val filePath = s"${AppConfig.baseFolder}formatted\\$origin - $destination ${DateTimeFormatter.ofPattern("ddMM").format(tripDate)}.txt"
+
+        Utils.saveToFile(filePath, formattedFares)
+    }
+  }
+
+  def getTripOptions(config: TripConfig): Seq[Seq[FlightDirection]] = {
+    for {
+      origin <- config.fromAirports
+      destination <- config.toAirports
+      date <- config.minDate to config.maxDate withStep Period.ofDays(1)
+      duration <- config.minDuration to config.maxDuration
+      flight = new FlightDirection(origin, destination, date)
+      returnFlight = new FlightDirection(destination, origin, date.plusDays(duration))
+    } yield Seq(flight, returnFlight)
+  }
+
+  def loadFares(route: Seq[FlightDirection]): Future[Seq[Fare]] = {
+    Log(s"Loading fares for route: '$route'...")
+
+    FaresProvider.OneTwoTrip.search(route) flatMap {
+      case Right(fares) =>
+        Log(s"${fares.length} fares for $route have been loaded")
+        Future(fares)
+      case Left(ex) =>
+        logError(route, ex.msg)
+        After(Duration.ofMinutes(1))(loadFares(route))
+    }
+  }
+
+  def loadFares(routes: Seq[Seq[FlightDirection]], maxConcurrentLoads: Int): Future[Seq[Fare]] = {
+    routes.grouped(maxConcurrentLoads).foldLeft(Future(Seq.empty[Fare])) {
+      (resultFuture, group) => {
+        for {
+          resultFares <- resultFuture
+          groupFares <- Future.sequence(group.map(route => loadFares(route))).map(_.flatten)
+        } yield resultFares ++ groupFares
+      }
+    }
+  }
+
+  def loadFares(config: TripConfig, maxConcurrentLoads: Int): Future[Seq[Fare]] = {
+    val tripOptions = getTripOptions(config)
+    loadFares(tripOptions, maxConcurrentLoads).andThen { case Success(allFares) => saveTripFares(allFares) }
+  }
+
+  def loadFares: Future[Seq[Fare]] = {
+    Log(s"Loading fares for ${AppConfig.tripConfigs.length} trip configurations...")
+
+    AppConfig.tripConfigs.foldLeft(Future(Seq.empty[Fare])) {
+      (resultFuture, config) =>
+        for {
+          resultFares <- resultFuture
+          configFares <- loadFares(config, MaxConcurrentLoads)
+        } yield resultFares ++ configFares
+    }
+  }
+
+  def scan(): Unit = {
+    val startTime = Instant.now
+
+    loadFares.onSuccess {
+      case fares =>
+        val endTime = Instant.now
+        val elapsedTime = Duration.between(startTime, endTime)
+        val timeToNextCheck = if (CheckInterval.minus(elapsedTime).isNegative) Duration.ofSeconds(1) else CheckInterval.minus(elapsedTime)
+        val nextCheckTime = LocalDateTime.now.plus(timeToNextCheck)
+
+        Log(s"${fares.length} have been successfully loaded")
+        Log(s"The next check is scheduled on $nextCheckTime")
+
+        After(timeToNextCheck)(Future(scan()))
+    }
+  }
+}
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,126 +1,34 @@
-import java.time.format.DateTimeFormatter
-import java.time.{Duration, Instant, LocalDateTime, Period}
+import java.time._
 
-import OneTwoTrip.Api.Route
-import OneTwoTrip.{Fare, LimitReachedException}
-import config.{AppConfig, TripConfig}
-import utils.Implicits._
-import utils.{After, Log, Utils}
+import flights.{FaresProvider, FlightDirection}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scala.util.Success
+import scala.util.{Failure, Success}
 
 object main extends App {
 
-  val MaxConcurrentLoads = 3
-  val CheckInterval = Duration.ofHours(24)
+  var route = Seq(
+    FlightDirection("SYD", "KBP", LocalDate.of(2016, 8, 12)),
+    FlightDirection("KBP", "SYD", LocalDate.of(2016, 9, 3)))
 
-  val cities = flights.ReferenceData.airports.map(a => (a.iataCode, a.city)).toMap
+  val providers = Seq(FaresProvider.Momondo, FaresProvider.OneTwoTrip)
 
-  def getFilePath(flights: Route): String = {
-    val route = flights.map(f => DateTimeFormatter.ofPattern("ddMM").format(f.date) + f.fromAirport + f.toAirport).mkString
-    val timestamp = DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmssZ").format(LocalDateTime.now)
-    val fileName = s"$timestamp $route.txt"
-
-    AppConfig.baseFolder + "formatted\\" + fileName
+  var response = providers.map(p => p.search(route)).foldLeft(Future(Seq.empty[flights.Fare])) {
+    (resultFuture, providerFuture) => for {
+      resultFares <- resultFuture
+      providerFares <- providerFuture.map(_.right.getOrElse(Seq.empty[flights.Fare])) recover { case res => Seq.empty[flights.Fare] }
+    } yield resultFares ++ providerFares
   }
 
-  def logError(route: Route, error: String) = Log(s"An error occurred when loading '$route': $error")
+  response.onComplete {
+    case Success(fares) =>
+      val groupedFares = fares.groupBy(f => f.itineraries).map(g => flights.Fare(g._1, g._2.head.date, g._2.flatMap(_.prices))).toSeq
+      println(s"${groupedFares.length} options found: ")
 
-  def saveTripFares(allFares: Seq[Fare]) = {
-    val faresByDirection = allFares.groupBy(f => {
-      val origin      = cities(f.flights.head.segments.head.fromAirport)
-      val destination = cities(f.flights.head.segments.last.toAirport)
-
-      (origin, destination)
-    })
-
-    faresByDirection.foreach {
-      case ((origin, destination), fares) =>
-        val formattedFares = fares.sortBy(_.price).map(f => f.prettyPrint).mkString("\n")
-        val tripDate = fares.head.flights.head.segments.head.departureDate
-        val filePath = s"${AppConfig.baseFolder}formatted\\$origin - $destination ${DateTimeFormatter.ofPattern("ddMM").format(tripDate)}.txt"
-
-        Utils.saveToFile(filePath, formattedFares)
-    }
+      groupedFares.sortBy(_.prices.map(_.price).min).foreach(f => println(f.prettyPrint))
+    case Failure(ex) => println(ex)
   }
-
-  def getTripOptions(config: TripConfig): Seq[Route] = {
-    for {
-      origin      <- config.fromAirports
-      destination <- config.toAirports
-      date        <- config.minDate to config.maxDate withStep Period.ofDays(1)
-      duration    <- config.minDuration to config.maxDuration
-      flight       = new OneTwoTrip.Direction(origin, destination, date)
-      returnFlight = new OneTwoTrip.Direction(destination, origin, date.plusDays(duration))
-    } yield Seq(flight, returnFlight)
-  }
-  
-  def loadFares(route: Route): Future[Seq[Fare]] = {
-    Log(s"Loading fares for route: '$route'...")
-
-    OneTwoTrip.Api.search(route) flatMap {
-      case Right(fares) =>
-        Log(s"${fares.length} fares for $route have been loaded")
-        Future(fares)
-      case Left(ex) => ex match {
-        case e: LimitReachedException =>
-          logError(route, "Requests limit reached")
-          After(Duration.ofMinutes(5))(loadFares(route))
-        case e =>
-          logError(route, e.getMessage)
-          After(Duration.ofMinutes(1))(loadFares(route))
-      }
-    }
-  }
-
-  def loadFares(routes: Seq[Route], maxConcurrentLoads: Int): Future[Seq[Fare]] = {
-    routes.grouped(maxConcurrentLoads).foldLeft(Future(Seq.empty[Fare])) {
-      (resultFuture, group) => {
-        for {
-          resultFares <- resultFuture
-          groupFares <- Future.sequence(group.map(route => loadFares(route))).map(_.flatten)
-        } yield resultFares ++ groupFares
-      }
-    }
-  }
-
-  def loadFares(config: TripConfig, maxConcurrentLoads: Int): Future[Seq[Fare]] = {
-    val tripOptions = getTripOptions(config)
-    loadFares(tripOptions, maxConcurrentLoads).andThen { case Success(allFares) => saveTripFares(allFares) }
-  }
-  
-  def loadFares: Future[Seq[Fare]] = {
-    Log(s"Loading fares for ${AppConfig.tripConfigs.length} trip configurations...")
-
-    AppConfig.tripConfigs.foldLeft(Future(Seq.empty[Fare])) {
-      (resultFuture, config) =>
-        for {
-          resultFares <- resultFuture
-          configFares <- loadFares(config, MaxConcurrentLoads)
-        } yield resultFares ++ configFares
-    }
-  }
-
-  def checkFares(): Unit = {
-    val startTime = Instant.now
-
-    loadFares.onSuccess {
-      case fares =>
-        val endTime = Instant.now
-        val elapsedTime = Duration.between(startTime, endTime)
-        val timeToNextCheck = if (CheckInterval.minus(elapsedTime).isNegative) Duration.ofSeconds(1) else CheckInterval.minus(elapsedTime)
-        val nextCheckTime = LocalDateTime.now.plus(timeToNextCheck)
-
-        Log(s"${fares.length} have been successfully loaded")
-        Log(s"The next check is scheduled on ${nextCheckTime}")
-
-        After(timeToNextCheck)(Future(checkFares()))
-    }
-  }
-
-  checkFares()
 
   scala.io.StdIn.readLine()
 }
