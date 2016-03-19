b448054da211469153fab0d48cc4e916be6c6b92
SNiko <SWashchyshchuk@gmail.com>
2016-03-19 19:45:56 +1100

    Convert prices in foreign currency to USD

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,5 +1,7 @@
 import java.time._
 
+import api.yahoo.Finance._
+import api.yahoo.Model._
 import flights.{FaresProvider, FlightDirection}
 import utils.Implicits._
 
@@ -11,19 +13,43 @@ import scalaz.EitherT
 object main extends App {
 
   var route = Seq(
-    FlightDirection("SYD", "KBP", LocalDate.of(2016, 8, 12)),
-    FlightDirection("KBP", "SYD", LocalDate.of(2016, 9, 3)))
+    FlightDirection("SYD", "KBP", LocalDate.of(2016, 12, 17)),
+    FlightDirection("KBP", "SYD", LocalDate.of(2017, 1, 15)))
 
   val providers = Seq(FaresProvider.Momondo, FaresProvider.OneTwoTrip)
 
-  var response = providers.map(p => p.search(route)).foldLeft(Future(Seq.empty[flights.Fare])) {
+  val faresFuture = providers.map(p => p.search(route)).foldLeft(Future(Seq.empty[flights.Fare])) {
     (resultFuture, providerFuture) => for {
       resultFares <- resultFuture
-      providerFares <- EitherT(providerFuture).leftMap { case err => println(err) } . getOrElse(Seq.empty[flights.Fare])
+      providerFares <- EitherT(providerFuture).leftMap { case err => println(err) }.getOrElse(Seq.empty[flights.Fare])
     } yield resultFares ++ providerFares
   }
 
-  response.onComplete {
+  val targetCurrency = "USD"
+
+  val faresInTargetCurrency = for {
+    fares <- faresFuture
+    exchangeRates <- {
+      val pairs = fares
+        .flatMap(f => f.prices.map(p => p.currency))
+        .filter(c => c != targetCurrency)
+        .distinct.map(c => CurrenciesPair(c, targetCurrency))
+
+      val rates = EitherT(getCurrencyRate(pairs)).map(q => q.query.results.rates)
+      rates.getOrElse(Seq.empty[api.yahoo.Model.Rate])
+    }
+    faresWithTargetCurrency = fares.map(fare => {
+      val pricesToConvert = fare.prices.filter(p => p.currency != targetCurrency)
+      val convertedPrices = pricesToConvert.map(p => {
+        val rate = exchangeRates.find(r => r.from(p.currency) && r.to(targetCurrency))
+        rate.map(r => flights.PriceInfo(p.price * r.rate.toDouble, targetCurrency, p.provider))
+      })
+
+      fare.copy(prices = fare.prices ++ convertedPrices.flatten)
+    })
+  } yield faresWithTargetCurrency
+
+  faresInTargetCurrency.onComplete {
     case Success(fares) =>
       val groupedFares = fares.groupBy(f => f.itineraries).map(g => flights.Fare(g._1, g._2.head.date, g._2.flatMap(_.prices))).toSeq
       println(s"${groupedFares.length} options found: ")
